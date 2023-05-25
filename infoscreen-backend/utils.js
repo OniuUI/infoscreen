96f35afc97257a36c307898ffce683efa9f1dfdd
MariusMagnussenOslo <marius.magnussen@ad.eyeshare.no>
2023-05-25 09:44:14 +0200

    moved annoying console message to debug

--- infoscreen-backend/utils.js
+++ infoscreen-backend/utils.js
@@ -1,12 +1,14 @@
 const getDb = require('./db').getDb;
+const debug = process.env.DEBUG || false;
 
 const removePastEvents = async () => {
     try {
         const db = getDb();
         const currentDate = new Date();
         const result = await db.collection('events').deleteMany({ eventDate: { $lt: currentDate } });
-
-        console.log(result.deletedCount + " events were removed");
+    if(debug){
+        console.log(result.deletedCount + " events were removed");   
+    }
     } catch (err) {
         console.error('Unable to remove past events:', err);
     }
