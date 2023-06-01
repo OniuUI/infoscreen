27f50a262fc29d6dc688f90f02f68cc9bc88af02
Marius Magnussen <marius.magnussen@ad.eyeshare.no>
2023-06-01 11:05:50 +0200

    forgot to commit this.

--- infoscreen-backend/utils.js
+++ infoscreen-backend/utils.js
@@ -5,14 +5,28 @@ const removePastEvents = async () => {
     try {
         const db = getDb();
         const currentDate = new Date();
-        const result = await db.collection('events').deleteMany({ eventDate: { $lt: currentDate } });
-    if(debug){
-        //console.log(result.deletedCount + " events were removed");
-    }
+
+        // Get all events
+        const events = await db.collection('events').find({}).toArray();
+
+        // Filter out IDs of events that are in the past
+        const pastEventIds = events
+            .filter(event => new Date(event.eventDate) < currentDate)
+            .map(event => event._id);
+
+        if (pastEventIds.length > 0) {
+            // Remove past events
+            const result = await db.collection('events').deleteMany({ _id: { $in: pastEventIds } });
+
+            if(debug){
+                //console.log(result.deletedCount + " events were removed");
+            }
+        }
     } catch (err) {
         console.error('Unable to remove past events:', err);
     }
 };
+
 
 module.exports = {
   removePastEvents,
