27d90aa598e3c2827c5fb5b2ad91584dd06e7d3c
Marius Magnussen <marius.magnussen@ad.eyeshare.no>
2024-04-24 00:15:08 +0200

    Removed logging on accesscontroller.

--- infoscreen-backend/controllers/accessController.js
+++ infoscreen-backend/controllers/accessController.js
@@ -4,11 +4,8 @@ exports.getManagersAndAdmins = async (req, res) => {
     try {
         const db = getDb();
         const roles = await db.collection('roles').find({ role: { $in: ['admin', 'manager'] } }).toArray();
-        console.log(roles);
         const userIds = roles.map(role => role.userId);
-        console.log(userIds);
         const users = await db.collection('users').find({ _id: { $in: userIds } }).toArray();
-        console.log(users);
         res.send({ users });
     } catch (err) {
         console.log(err);
