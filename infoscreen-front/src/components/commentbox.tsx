3f545b5b207d60d1cc52a5984d235b4e791d91d8
Marius Magnussen <marius.magnussen@ad.eyeshare.no>
2024-04-23 16:22:17 +0200

    MINOR: fixed add comments to update states.

--- infoscreen-front/src/components/commentbox.tsx
+++ infoscreen-front/src/components/commentbox.tsx
@@ -1,13 +1,36 @@
 import React, {useState, useRef, SetStateAction, Dispatch} from 'react';
+import {v4 as uuidv4} from "uuid";
+import {apiService} from "./api/apiservice";
+import {Task} from "./utils/types";
 
 interface CommentBoxProps {
-    handleAddComment: (commentText: string) => Promise<void>;
+    task: Task;
+    setTask: Dispatch<SetStateAction<Task>>;
+
 }
 
-const CommentBox: React.FC<CommentBoxProps> = ({ handleAddComment }) => {
+const CommentBox: React.FC<CommentBoxProps> = ({ task, setTask }) => {
+    const [localTask, setLocalTask] = useState<Task>(task);
     const [comment, setComment] = useState('');
     const [isCommentFieldFocused, setIsCommentFieldFocused] = useState(false);
     const timer = useRef<NodeJS.Timeout | null>(null);
+
+    const handleAddComment = async (commentText: string) => {
+        try {
+            const comment = { id: uuidv4(), text: commentText, author: 'User' }; // Replace 'User' with the actual user
+            const response = await apiService.post(`/kaizen/addComment/${localTask.id}`, comment);
+            console.log('Response:', response);
+            if (response.success) {
+                // Update the task state with the new comment
+               // localTask.comments.push(response.comment);
+                console.log(localTask.comments)
+                console.log(response.comment)
+                setTask((prevTask: Task) => ({...prevTask, comments: [...prevTask.comments, response.comment]}));
+            }
+        } catch (error) {
+            console.error('Failed to add comment:', error);
+        }
+    };
 
     const handleCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setComment(event.target.value);
