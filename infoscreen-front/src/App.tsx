9b11dc31b9e7b69a6f214875fd35a9565681f519
Marius Magnussen <marius@oniu.no>
2024-07-14 18:37:03 +0200

    Provider login

--- infoscreen-front/src/App.tsx
+++ infoscreen-front/src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from "./components/mainbody";
@@ -10,27 +9,28 @@ import Hub from "./components/hub/hub";
 import Kaizen from "./components/kaizen";
 import Kaizenview from "./components/kaizenview";
 import KaizenSettings from "./components/admin/kaizensettings";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Container />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/thirsty" element={<Thirsty />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/hub" element={<Hub />} />
-          <Route path="/hub/profile" element={<Profile />} />
-          <Route path="/kaizen" element={<Kaizen />} />
-          <Route path="/kaizen/settings" element={<KaizenSettings />} />
-          <Route path="/kaizenview" element={<Kaizenview />}
-          />
-        </Routes>
-      </div>
-    </Router>
-    );
+      <GoogleOAuthProvider clientId="717776026496-jdn21t7kkruprnn967ds5jgb7temicj6.apps.googleusercontent.com">
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Container />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/thirsty" element={<Thirsty />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/hub" element={<Hub />} />
+              <Route path="/hub/profile" element={<Profile />} />
+              <Route path="/kaizen" element={<Kaizen />} />
+              <Route path="/kaizen/settings" element={<KaizenSettings />} />
+              <Route path="/kaizenview" element={<Kaizenview />} />
+            </Routes>
+          </div>
+        </Router>
+      </GoogleOAuthProvider>
+  );
 };
 
 export default App;
--- infoscreen-front/src/components/login.tsx
+++ infoscreen-front/src/components/login.tsx
@@ -1,6 +1,8 @@
 import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'; // Updated import for GoogleLogin
+import MicrosoftLogin from 'react-microsoft-login'; // Updated import for MicrosoftLogin
 import LoginSuccess from "./LoginSuccess";
 import { API_BASE_URL } from '../apiConfig';
 import bgImage from '../components/img/bg.png';
@@ -9,17 +11,40 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoginSuccess, setIsLoginSuccess] = useState(false);
-  const [isLoginFailure, setIsLoginFailure] = useState(false); // New state
+  const [isLoginFailure, setIsLoginFailure] = useState(false);
   const navigate = useNavigate();
   let logoutTimer: any;
 
   useEffect(() => {
+    // Cleanup on component unmount
     return () => {
-      if(logoutTimer) {
+      if (logoutTimer) {
         clearTimeout(logoutTimer);
       }
-    }
+    };
   }, []);
+
+  const handleGoogleSuccess = (response: any) => {
+    console.log('Google login success:', response);
+    // Process login success, e.g., setting user state, redirecting, etc.
+  };
+
+  // Handler for Google login failure
+  const handleGoogleFailure = () => {
+    console.error('Google login failed');
+    // Additional logic to handle the error can be added here
+  };
+
+  // Handler for Microsoft login success/failure
+  const handleMicrosoftResponse = (err: any, data: any, msal: any) => {
+    if (err) {
+      console.error('Microsoft login failed:', err);
+      // Process login failure
+    } else {
+      console.log('Microsoft login success:', data);
+      // Process login success
+    }
+  };
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -31,112 +56,104 @@ const Login = () => {
         password,
       });
 
-      // If successful, store the auth tokens and role
       if (response.data) {
-        localStorage.setItem('accessToken', response.data.accessToken);
-        localStorage.setItem('refreshToken', response.data.refreshToken);
-        localStorage.setItem('userIdent', response.data.userIdent);
-        localStorage.setItem('userRole', response.data.role); // Set the role in local storage
-
-        if(logoutTimer) {
-          clearTimeout(logoutTimer);
-        }
-
-        logoutTimer = setTimeout(() => {
-          localStorage.removeItem('accessToken');
-          localStorage.removeItem('refreshToken');
-          localStorage.removeItem('userIdent');
-          localStorage.removeItem('userRole'); // Remove the role from local storage when the token expires
-        }, response.data.expiresIn * 1000);
-
+        // Handle normal user login success
         setIsLoginSuccess(true);
         setTimeout(() => navigate('/'), 3000); // Redirect after 3 seconds
       }
     } catch (error) {
       console.error('Error during authentication:', error);
-      setIsLoginFailure(true); // Set the failure state on login failure
-      setTimeout(() => setIsLoginFailure(false), 1500); // Reset the failure state after 1.5 seconds
+      setIsLoginFailure(true);
     }
   };
 
-  // Add the shake class to the container div when login fails
-  const containerClasses = isLoginFailure ? 'shake' : '';
-
   return (
-    <div
-      style={{
-        backgroundImage: `url(${bgImage})`,
-        backgroundSize: 'cover',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {isLoginSuccess ? (
-        <LoginSuccess />
-      ) : (
-        <div
-          className={containerClasses}
+      <div
           style={{
-            backgroundColor: 'white',
-            padding: '2em',
-            borderRadius: '8px',
-            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+            backgroundImage: `url(${bgImage})`,
+            backgroundSize: 'cover',
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
-        >
-          <form onSubmit={handleSubmit}>
-            <input
-              type="email"
-              value={email}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-              placeholder="Email"
-              style={{
-                marginBottom: '1em',
-                padding: '0.5em',
-                width: '100%',
-                border: '1px solid #ccc',
-                borderRadius: '4px',
-              }}
-            />
-            <input
-              type="password"
-              value={password}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-              placeholder="Password"
-              style={{
-                marginBottom: '1em',
-                padding: '0.5em',
-                width: '100%',
-                border: '1px solid #ccc',
-                borderRadius: '4px',
-              }}
-            />
-            <button
-              type="submit"
-              style={{
-                backgroundColor: 'green',
-                color: 'white',
-                padding: '0.75em',
-                width: '100%',
-                border: 'none',
-                borderRadius: '4px',
-                cursor: 'pointer',
-              }}
+      >
+        {isLoginSuccess ? (
+            <LoginSuccess />
+        ) : (
+            <div
+                style={{
+                  backgroundColor: 'white',
+                  padding: '2em',
+                  borderRadius: '8px',
+                  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+                }}
             >
-              Login
-            </button>
+              <GoogleLogin
+                  onSuccess={handleGoogleSuccess}
+                  onError={handleGoogleFailure}
+              />
+              <div>Microsoft login</div>
+              <MicrosoftLogin
+                  clientId="YOUR_MICROSOFT_CLIENT_ID" // Replace with your actual Microsoft client ID
+                  authCallback={handleMicrosoftResponse}
+              >
+                <button style={{ backgroundColor: '#2F2F2F', color: 'white', padding: '10px', borderRadius: '5px' }}>
+                  Login with Microsoft
+                </button>
+              </MicrosoftLogin>
 
-            {isLoginFailure && (
-              <p style={{ color: 'red', marginTop: '1em' }}>
-                Incorrect email or password.
-              </p>
+              <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    style={{
+                      marginBottom: '1em',
+                      padding: '0.5em',
+                      width: '100%',
+                      border: '1px solid #ccc',
+                      borderRadius: '4px',
+                    }}
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    style={{
+                      marginBottom: '1em',
+                      padding: '0.5em',
+                      width: '100%',
+                      border: '1px solid #ccc',
+                      borderRadius: '4px',
+                    }}
+                />
+                <button
+                    type="submit"
+                    style={{
+                      backgroundColor: 'green',
+                      color: 'white',
+                      padding: '0.75em',
+                      width: '100%',
+                      border: 'none',
+                      borderRadius: '4px',
+                      cursor: 'pointer',
+                    }}
+                >
+                  Login
+                </button>
+              </form>
+              {isLoginFailure && (
+                  <p style={{ color: 'red', marginTop: '1em' }}>
+                    Incorrect email or password.
+                  </p>
               )}
-          </form>
-        </div>
+            </div>
         )}
-    </div>
-    );
+      </div>
+  );
 };
 
 export default Login;
